53bb535755a1250997f61e6046b3ce955e98c09a
Ryohji Ikebe <Ryohji.Ikebe@gmail.com>
2020-03-08 23:29:21 +0900

    reorder functions and reword comments

--- sudoku-sat.c
+++ sudoku-sat.c
@@ -17,14 +17,13 @@
 /** caluculate the length of the array */
 #define LENGTH_OF(a) (sizeof(a) / sizeof((a)[0]))
 
-typedef struct indices indices_t;
-
 /** bunch of the indices. to return as the value of functions */
+typedef struct indices indices_t;
 struct indices {
   index_t index[N];
 };
 
-/** read the values of sudoku */
+/** read the numbers of sudoku */
 static int* read_sudoku(FILE* fp, int sudoku[N * N]);
 
 /** count the number of constraints of specified sudoku */
@@ -34,84 +33,37 @@ static int count_constraint(const int sudoku[N * N]);
  * convert specified sudoku to the constraint into specified FILE
  *
  * @param fp is the FILE, converted constraint output to.
+ * @param n is the column offset, previous output ended on.
  * @param sudoku is the sudoku problem to solve.
- * @param n is the column offset, previous output ended on.
  * @return the column offset this function output ended on.
  */
-static int to_constraint(FILE* fp, const int sudoku[N * N], int n);
+static int write_specific_constraint(FILE* fp, int n, const int sudoku[N * N]);
+
+/** write sudoku universal constraint into specified FILE */
+static int write_generic_constraint(FILE* fp, int n);
 
 /**
- * convert given indices to `at least one` constraint into specified FILE
+ * entry point
  *
- * @param fp is the FILE, converted constraint output to.
- * @param indices turned into `at least one` constraint.
- * @param n is the column offset, previous output ended on.
- * @return the column offset this function output ended on.
+ * read sudoku and convert it into DIMACS
  */
-static int at_least_one_of(FILE* fp, indices_t indices, int n);
-
-/**
- * convert given indices to `at most one` constraint into specified FILE
- *
- * @param fp is the FILE, converted constraint output to.
- * @param indices turned into `at most one` constraint.
- * @param n is the column offset, previous output ended on.
- * @return the column offset this function output ended on.
- */
-static int at_most_one_of(FILE* fp, indices_t indices, int n);
-
-static indices_t num_for(int row, int col);
-static indices_t row_for(int row, int num);
-static indices_t col_for(int col, int num);
-static indices_t box_for(int box, int num);
-
-static int fput_index(FILE* fp, index_t i, int n);
-
 int main() {
-  int pairs[N * N][2];
-  int(*p)[2] = pairs;
-  for (int y = 0; y < N; y += 1) {
-    for (int x = 0; x < N; x += 1) {
-      (*p)[0] = x;
-      (*p)[1] = y;
-      p += 1;
-    }
-  }
+  const int* sudoku = read_sudoku(stdin, (int[N * N]){0});
 
-  int* sudoku = read_sudoku(stdin, (int[N * N]){0});
-
+  // output DIMACS problem definition line
   fprintf(stdout, "p cnf %d %d\n", N * N * N,
           count_constraint(sudoku) + N * N * (1 + nC2) * 4);
 
-  int c = to_constraint(stdout, sudoku, 0);
-
-  for (p = pairs; p != END_OF(pairs); p += 1) {
-    indices_t indices = num_for((*p)[0], (*p)[1]);
-    c = at_least_one_of(stdout, indices, c);
-    c = at_most_one_of(stdout, indices, c);
-  }
-
-  for (p = pairs; p != END_OF(pairs); p += 1) {
-    indices_t indices = row_for((*p)[0], (*p)[1]);
-    c = at_least_one_of(stdout, indices, c);
-    c = at_most_one_of(stdout, indices, c);
-  }
-
-  for (p = pairs; p != END_OF(pairs); p += 1) {
-    indices_t indices = col_for((*p)[0], (*p)[1]);
-    c = at_least_one_of(stdout, indices, c);
-    c = at_most_one_of(stdout, indices, c);
-  }
-
-  for (p = pairs; p != END_OF(pairs); p += 1) {
-    indices_t indices = box_for((*p)[0], (*p)[1]);
-    c = at_least_one_of(stdout, indices, c);
-    c = at_most_one_of(stdout, indices, c);
-  }
+  // convert sudoku into DIMACS
+  int c = write_specific_constraint(stdout, 0, sudoku);
+  (void)write_generic_constraint(stdout, c);
 
   return 0;
 }
 
+/*
+ reader part
+ */
 int* read_sudoku(FILE* fp, int buffer[N * N]) {
   int* p = buffer;
   while (p != buffer + N * N) {
@@ -121,6 +73,9 @@ int* read_sudoku(FILE* fp, int buffer[N * N]) {
   return buffer;
 }
 
+/*
+ writer part
+ */
 int count_constraint(const int buffer[N * N]) {
   int n = 0;
   for (const int* p = buffer; p != buffer + N * N; p += 1) {
@@ -129,30 +84,91 @@ int count_constraint(const int buffer[N * N]) {
   return n;
 }
 
-int to_constraint(FILE* fp, const int sudoku[N * N], int col) {
+/** current column aware formatter of index_t */
+static int fput_index(FILE* fp, int n, index_t i);
+
+// fixed number gives powerful constraint.
+int write_specific_constraint(FILE* fp, int col, const int sudoku[N * N]) {
   for (int i = 0; i < N * N; i += 1) {
     const int n = sudoku[i];
     if (0 < n && n <= N) {
-      col = fput_index(fp, index_of(i / N, i % N, n - 1), col);
-      col = fput_index(fp, 0, col);
+      col = fput_index(fp, col, index_of(i / N, i % N, n - 1));
+      col = fput_index(fp, col, 0);
     }
   }
   return col;
 }
 
-int at_least_one_of(FILE* fp, indices_t indices, int n) {
-  for (int i = 0; i < N; i += 1) {
-    n = fput_index(fp, indices.index[i], n);
-  }
-  return fput_index(fp, 0, n);
+/*
+ generic constraint constructed with 4 part; cell, row, column, and box.
+ */
+static int write_constraints_of(FILE* fp, int n, indices_t (*)(int, int));
+
+static indices_t cell_for(int row, int col);
+static indices_t row_for(int row, int num);
+static indices_t col_for(int col, int num);
+static indices_t box_for(int box, int num);
+
+int write_generic_constraint(FILE* fp, int n) {
+  n = write_constraints_of(stdout, n, cell_for);
+  n = write_constraints_of(stdout, n, row_for);
+  n = write_constraints_of(stdout, n, col_for);
+  n = write_constraints_of(stdout, n, box_for);
+  return n;
 }
 
-int at_most_one_of(FILE* fp, indices_t indices, int n) {
+/*
+ each generic constraint constraint builder; it makes exactly one proposition
+ true. in another word, it choose at least one proposition and at most one
+ proposition.
+ */
+static void pair_initialize_exec(int (*pairs)[2]);
+static int at_least_one_of(FILE* fp, int n, indices_t indices);
+static int at_most_one_of(FILE* fp, int n, indices_t indices);
+
+int write_constraints_of(FILE* fp, int n, indices_t (*calculate)(int, int)) {
+  static int pairs[N * N][2];
+  static void (*pair_initialize)(int(*pairs)[2]) = pair_initialize_exec;
+  if (pair_initialize) {
+    pair_initialize(pairs);
+    pair_initialize = NULL;
+  }
+
+  for (int(*p)[2] = pairs; p != END_OF(pairs); p += 1) {
+    indices_t indices = calculate((*p)[0], (*p)[1]);
+    n = at_least_one_of(stdout, n, indices);
+    n = at_most_one_of(stdout, n, indices);
+  }
+
+  return n;
+}
+
+void pair_initialize_exec(int (*pairs)[2]) {
+  int(*p)[2] = pairs;
+  for (int y = 0; y < N; y += 1) {
+    for (int x = 0; x < N; x += 1) {
+      (*p)[0] = x;
+      (*p)[1] = y;
+      p += 1;
+    }
+  }
+}
+
+/** convert given indices into `at least one` constraint */
+int at_least_one_of(FILE* fp, int n, indices_t indices) {
+  for (int i = 0; i < N; i += 1) {
+    n = fput_index(fp, n, indices.index[i]);
+  }
+  return fput_index(fp, n, 0);
+}
+
+/** convert given indices into `at most one` constraint */
+int at_most_one_of(FILE* fp, int n, indices_t indices) {
   for (int i = 1; i < N; i += 1) {
     for (int j = i; j < N; j += 1) {
-      n = fput_index(fp, -indices.index[i - 1], n);
-      n = fput_index(fp, -indices.index[j], n);
-      n = fput_index(fp, 0, n);
+      n = fput_index(fp, n, -indices.index[i - 1]);
+      n = fput_index(fp, n, -indices.index[j]);
+      n = fput_index(fp, n, 0);
     }
   }
   return n;
@@ -170,7 +186,7 @@ static index_t calculate_num(void* cookie, int i) {
   return index_of(((int*)cookie)[0], ((int*)cookie)[1], i);
 }
 
-indices_t num_for(int row, int col) {
+indices_t cell_for(int row, int col) {
   return make_indeices((int[2]){row, col}, calculate_num);
 }
 
@@ -202,14 +218,15 @@ indices_t box_for(int box, int num) {
                        calculate_box);
 }
 
-static char* make_format();
-static char* refer_format();
-static char format_[16];
-static char* (*format)() = make_format;
+int fput_index(FILE* fp, int col, index_t i) {
+  static char format[16] = {0};
+  if (!format[0]) {
+    sprintf(format, "-%d", N * N * N);
+    sprintf(format, "%%%lud", strlen(format));
+  }
 
-int fput_index(FILE* fp, index_t i, int col) {
   char s[16];
-  int n = sprintf(s, format(), i);
+  int n = sprintf(s, format, i);
   if (col != 0) {
     int c;
     if (col + 1 + n > LINE_WIDTH) {
@@ -222,15 +239,4 @@ int fput_index(FILE* fp, index_t i, int col) {
   }
   fputs(s, fp);
   return n;
-}
-
-char* make_format() {
-  format = refer_format;
-  sprintf(format_, "%d", -N * N * N);
-  sprintf(format_, "%%%lud", strlen(format_));
-  return format_;
-}
-
-char* refer_format() {
-  return format_;
 }
